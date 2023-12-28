87525ad0b710cd0e5ecf95b86d47284c53f99448
BappaBanerjee <bappa3727@gmail.com>
2023-12-28 11:42:15 +0530

    image crousel component

--- app/page.js
+++ app/page.js
@@ -1,19 +1,44 @@
+"use client";
+
 import Imagecard from "@/components/Imagecard";
 import Card from "@/components/card";
 import React from "react";
 
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+let settings = {
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+};
+
+const nfts = [
+  "/assets/images/nft1.jpg",
+  "/assets/images/nft2.jpg",
+  "/assets/images/nft3.jpg",
+  "/assets/images/nft4.jpg",
+  "/assets/images/nft5.jpg",
+  "/assets/images/nft6.jpg",
+  "/assets/images/nft7.jpg",
+  "/assets/images/nft8.jpg",
+];
+
+let key = 1;
+
 const App = () => {
   return (
     <section>
-      <div className="border grid grid-cols-3 gap-4 justify-between border-transparent">
-        <Imagecard />
-        <Imagecard />
-        <Imagecard />
-        <Imagecard />
-        <Imagecard />
-        <Imagecard />
-        <Imagecard />
-      </div>
+      <Slider {...settings}>
+        {nfts.map((index) => (
+          <Imagecard image={index} key={key++} />
+        ))}
+      </Slider>
+      {/* <div className="border grid grid-cols-3 gap-4 justify-between border-transparent"> */}
     </section>
   );
 };
--- components/Imagecard.jsx
+++ components/Imagecard.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 import Image from "next/image";
 
-const Imagecard = () => {
+const Imagecard = ({ image, key }) => {
   return (
-    <div className=" relative bg-background-color-1 rounded-lg p-4">
+    <div className="relative bg-background-color-1 rounded-lg p-4 flex-center justify-center">
       <div className="overflow-hidden rounded-lg">
         <Image
-          src="/assets/images/dogNFT.jpg"
+          src={image}
           alt="nft"
-          height={400}
-          width={400}
+          key={key}
+          height={300}
+          width={300}
           className="rounded-lg hover:scale-110 duration-700"
         />
       </div>
-
-      <div className="absolute bottom-8 left-8">Great NFT!</div>
+      <div className="absolute bottom-14 left-14">Great NFT!</div>
     </div>
   );
 };
